f9c8eabe54999479c574747c49888aec9e768daa
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-06-26 05:11:43 +0000

    Add CreateCommand class and handler and create folder structure for the others commands.

---
+++ src/Core/HrLeaveManagementApplication/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+
+namespace HrLeaveManagementApplication;
+
+public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        //Validate incoming data
+
+        //Convert to domain entity object
+        var leaveTypeToCreate = _mapper.Map<HrLeaveManagementDomain.LeaveType>(request);
+
+        //Add to datebase
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+
+        //Return record id
+        return leaveTypeToCreate.Id;
+    }
+}
